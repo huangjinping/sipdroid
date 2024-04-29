b14a95927a756bb5e979f7c53ff50e320bccb224
harris1000 <huangjinping1000@163.com>
2024-04-29 18:21:53 +0800

    1
    1

---
+++ app/src/main/java/com/jstun/NewsActivity.java
@@ -0,0 +1,15 @@
+package com.jstun;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.sipdroid.sipua.R;
+
+public class NewsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
